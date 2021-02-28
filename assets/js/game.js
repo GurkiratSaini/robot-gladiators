ea22fbd3ab07ee2a036fdd2763c14cd436673503
Gurkirat Saini <78436028+GurkiratSaini@users.noreply.github.com>
2021-02-28 17:29:22 -0500

    added randomness to fight start sequence

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,47 +7,61 @@
 
 
 var fight = function (enemy) {
+
+    // keep track of who goes first
+    var isPlayerTurn = true;
+
+    if (Math.random() > 0.5) {
+        isPlayerTurn = false;
+    }
     // repeat and execute as long as the enemy-robot is alive
     while (enemy.health > 0 && playerInfo.health > 0) {
 
-        if(fightOrSkip()){
-            break;
+        if (isPlayerTurn) {
+            if (fightOrSkip()) {
+                break;
+            }
+
+
+
+            var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
+            enemy.health = Math.max(0, enemy.health - damage);
+            console.log(
+                playerInfo.name + " attacked " + enemy.name + ". " + enemy.name + " now has " + enemy.health + " health remaining."
+            );
+
+            // check enemy's health
+            if (enemy.health <= 0) {
+                window.alert(enemy.name + " has died!");
+
+                playerInfo.money = playerInfo.money + 20;
+                break;
+            }
+            else {
+                window.alert(enemy.name + " still has " + enemy.health + " health left.");
+            }
         }
-        
-
-        var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
-        enemy.health = Math.max(0, enemy.health - damage);
-        console.log(
-            playerInfo.name + " attacked " + enemy.name + ". " + enemy.name + " now has " + enemy.health + " health remaining."
-        );
-
-        // check enemy's health
-        if (enemy.health <= 0) {
-            window.alert(enemy.name + " has died!");
-
-            playerInfo.money = playerInfo.money + 20;
-            break;
-        }
+        // player gets attacked first
         else {
-            window.alert(enemy.name + " still has " + enemy.health + " health left.");
-        }
 
-        // remove player's health by subtracting the amount ser in the enemy.attack variable
+            // remove player's health by subtracting the amount ser in the enemy.attack variable
 
-        var damage = randomNumber(enemy.attack - 3, enemy.attack);
-        playerInfo.health = Math.max(0, playerInfo.health - damage);
-        console.log(
-            enemy.name + " attacked " + playerInfo.name + ". " + playerInfo.name + " now has " + playerInfo.health + " health remaining."
-        );
+            var damage = randomNumber(enemy.attack - 3, enemy.attack);
+            playerInfo.health = Math.max(0, playerInfo.health - damage);
+            console.log(
+                enemy.name + " attacked " + playerInfo.name + ". " + playerInfo.name + " now has " + playerInfo.health + " health remaining."
+            );
 
-        // check player's health
-        if (playerInfo.health <= 0) {
-            window.alert(playerInfo.name + " has died.");
-            break;
+            // check player's health
+            if (playerInfo.health <= 0) {
+                window.alert(playerInfo.name + " has died.");
+                break;
+            }
+            else {
+                window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.")
+            }
         }
-        else {
-            window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.")
-        }
+        isPlayerTurn = !isPlayerTurn;
     }
 
 
@@ -142,19 +156,19 @@ var randomNumber = function (min, max) {
     return value;
 }
 
-var getPlayerName = function() {
+var getPlayerName = function () {
     var name = "";
-  
-    while (name === "" || name === null ) {
-      name = prompt("What is your robot's name?");
+
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name?");
     }
-  
-  
+
+
     console.log("Your robot's name is " + name);
     return name;
-  }
+}
 
-var fightOrSkip = function() {
+var fightOrSkip = function () {
 
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
     promptFight = promptFight.toLowerCase();
